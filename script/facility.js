f6cf9fa39a238bf0ceacc044b742d4b1d2d60105
0x-ERROR <35882512+0x-ERROR@users.noreply.github.com>
2024-06-09 07:13:32 +1000

    AAAAAAAAAAAAAAAAAA

--- script/facility.js
+++ script/facility.js
@@ -10,8 +10,14 @@ const facility_choice = [
     " It reads: ",
     "\"You were thinking, what's out there can't possibly be any worse than what's in here, right?\""
 ]
-const stage_one_end = [
-    "To be continued."
+const facility_a = [
+    "As you look forward, you realise closing your eyes and keeping them open has no effect on what you can see.",
+    " Standing in the rays of what little light remains, you look back at the crumpled note once more.",
+    " Just barely, you are able to see faint black blocks on the other side of the note.",
+    " You flip it over. ",
+    //br
+    " It reads:"
+    //glitch and cipher
 ]
 
 function delayCalc(type, string="", speed = 50) {
@@ -41,6 +47,13 @@ function waitForMs(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+function glitchEffect() {
+    glitch.style.visibility = "visible"
+    setTimeout(() => {
+        glitch.style.visibility = "hidden"
+    }, 300);
+}
+
 if (localStorage.getItem("stage") == "2a") {
     consoleText.style.fontFamily = "Times New Roman";
     let delay = 500
@@ -55,7 +68,23 @@ if (localStorage.getItem("stage") == "2a") {
     setTimeout(typeSentence, delay, facility_choice[7], consoleText, 30)
     delay += delayCalc("typewriter", facility_choice[7], 30)
 
-    //tbc
-    setTimeout(()=>{consoleText.innerHTML += "<br><br>"}, delay)
-    setTimeout(typeSentence, delay, stage_one_end[0], consoleText, 30)
+    for (let i = 0; i < 5; i++) {
+        if (i == 0 || i == 4) {
+            setTimeout(()=>{consoleText.innerHTML += "<br><br>"}, delay)
+        }
+        setTimeout(typeSentence, delay, facility_a[i], consoleText, 30)
+        delay += delayCalc("typewriter", facility_a[i], 30)
+    }
+
+    setTimeout(glitchEffect, delay)
+    delay += 100
+    setTimeout(()=> {
+        consoleText.innerHTML = '';
+        consoleText.style.color = "#e0e0e0";
+        consoleText.style.fontFamily = "Times New Roman";
+        window.location.href = "facilitycipher.html";
+    }, delay)
+    
+    //cipher text
+    
 }
